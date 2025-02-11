8368bd1a2c211715d65cbffb993ded46b70fd603
waimea-zslatham <zslatham@waimea.school.nz>
2025-02-11 15:08:25 +1300

    Project Set-up

---
+++ src/Main.kt
@@ -0,0 +1,27 @@
+/**
+ * A little guessing game in Kotlin
+ */
+
+
+fun main() {
+    println("---------------------")
+    println(" Guessing Game 2000 ")
+    println("-----------------------")
+    println()
+
+    val numToGuess = (1..100).random()
+
+    println("I'm thinking of a number from 1 - 100")
+    println("Can you guess it?")
+
+    // MAIN LOOP
+    while (true) {
+        // Ask the user for a  guess
+
+        // Check if it's correct
+
+        // Break out of the loop if correct
+
+        // Otherwise give high/lower feedback
+    }
+}
