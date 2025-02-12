c2252d486aafec3f5d19b214a0ad2d7e1d130ca3
waimea-zslatham <zslatham@waimea.school.nz>
2025-02-13 12:22:30 +1300

    creating it. still need score done

--- src/Main.kt
+++ src/Main.kt
@@ -14,14 +14,35 @@ fun main() {
     println("I'm thinking of a number from 1 - 100")
     println("Can you guess it?")
 
+    val scoreText = "1000"
+    var score = scoreText.toIntOrNull()
+    println("Score: $score")
+
     // MAIN LOOP
     while (true) {
         // Ask the user for a  guess
+        val guess = readln().toIntOrNull()
 
+        if (guess == null) {
+            println("Enter a number!")
+            continue
+        }
+        if (guess == numToGuess) {
+            println("Yeah you got it!")
+           val score = 1
+            println("Score: $score")
+            break
+        }
+        else if (guess > numToGuess)
+            println("You have guessed too high")
+        else
+            println("You have guessed too low")
+        
         // Check if it's correct
 
         // Break out of the loop if correct
 
         // Otherwise give high/lower feedback
+
+        }
     }
-}
